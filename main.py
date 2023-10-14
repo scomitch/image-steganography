c851d57557f6cd858b1f9eda051ff9756d7d3058
theguildy <iain.guild.2020@uni.strath.ac.uk>
2023-10-14 15:49:37 +0100

    Menu updated to include hide and extract options

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+import os
+from importlib import resources
+
+import LSBExtract
 import LSBHide
 
 print("Welcome to the Data Smuggling application!")
+
+def list_images():
+    all_images = [f for f in os.listdir('resources/out') if os.path.isfile((os.path.join('resources/out'), f))]
+
+    if not all_images:
+        print("There are no images with smuggled data currently stored.")
+        return None
+
+    for index, filename in enumerate(all_images, 1):
+        print(f"{index+1}: {filename}")
+
+    while True:
+        try:
+            img_choice = int(input("Please choose from the above options: "))
+            if 1 <= img_choice <= len(all_images):
+                selected_image = all_images[img_choice - 1]
+                print(f"Selected Image: {selected_image}")
+                return os.path.join('resources/out/', selected_image)
+            else:
+                print("Invalid Selection. Please try again.")
+        except:
+            print("Please enter a numerical option as listed.")
 
 while True:
     print("Please Choose from the following:"
@@ -13,6 +39,11 @@ while True:
         message = input("Please enter the string you wish to smuggle:\n:- ")
         LSBHide.smuggle_string(message)
     elif choice == 2:
-        break
+        LSBExtract.extract_message(list_images())
     else:
         print("Your choice was invalid, please try again")
+
+
+
+
+
