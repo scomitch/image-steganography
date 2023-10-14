0e57312d4571f6732be9b70d93c9fd266cb9838c
theguildy <iain.guild.2020@uni.strath.ac.uk>
2023-10-14 14:27:39 +0100

    Initial commit with basic user interface

---
+++ cli/main.py
@@ -0,0 +1,15 @@
+print("Welcome to the Data Smuggling application!")
+
+while True:
+    print("Please Choose from the following:"
+          "\n1: Hide a String in the image"
+          "\n2: Extract a hidden string from an image")
+
+    choice = input(":- ")
+
+    if choice == 1:
+        break
+    elif choice == 2:
+        break
+    else:
+        print("Your choice was invalid, please try again")
