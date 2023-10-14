4d55445f87a1af8f2c21f2c473670037d6710a61
PClancyy <36280985+PClancyy@users.noreply.github.com>
2023-10-14 16:17:16 +0100

    image show horizontal

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from PIL import Image
 
-# print("Welcome to the Data Smuggling application!")
-#
-# while True:
-#     print("Please Choose from the following:"
-#           "\n1: Hide a String in the image"
-#           "\n2: Extract a hidden string from an image")
-#
-#     choice = input(":- ")
-#
-#     if choice == 1:
-#         break
-#     elif choice == 2:
-#         break
-#     else:
-#         print("Your choice was invalid, please try again")
+print("Welcome to the Data Smuggling application!")
+
+while True:
+    print("Please Choose from the following:"
+          "\n1: Hide a String in the image"
+          "\n2: Extract a hidden string from an image"
+          "\n3: Show original & altered image")
+
+    choice = input(":- ")
+
+    if choice == 1:
+        break
+    elif choice == 2:
+        break
+    elif choice == 3:
+        break
+    else:
+        print("Your choice was invalid, please try again")
 
 
 def check_constraints(image_path, string_to_hide):
