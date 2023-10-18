2de1a073db391b092a58a3ba1580bdcd6590c7f3
theguildy <iain.guild.2020@uni.strath.ac.uk>
2023-10-18 18:01:45 +0100

    file list sorted in menu option 2

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import os
 import sys
 
-from PIL import Image
-
 import LSBExtract
 import LSBHide
 # import image_show
 
 
 def menu():
-    print("Welcome to the Data Smuggling application!")
-    print("Please Choose from the following:"
+    print("Main Menu - Please Choose from the following:"
           "\n1: Hide a String in the image"
           "\n2: Extract a hidden string from an image"
           "\n3: Show original & altered image"
@@ -21,27 +18,27 @@ def menu():
     if int(choice) == 1:
         message = input("Please enter the string you wish to smuggle:\n:- ")
         LSBHide.smuggle_string(message)
+        menu()
     elif int(choice) == 2:
         list_images()
     elif int(choice) == 3:
         print("todo")
         # image_show
     elif int(choice) == 4:
+        print("\n\nGoodbye!")
         sys.exit()
     else:
         print("Your choice was invalid, please try again")
 
-1
+
 def list_images():
-
-    # print(LSBExtract.extract_message('resources/out/out.bmp'))
-
-
     all_images = [f for f in os.listdir('resources/out') if os.path.isfile(os.path.join('resources/out', f))]
 
     if not all_images:
         print("There are no images with smuggled data currently stored.")
         return None
+
+    all_images.sort()
 
     for index, filename in enumerate(all_images, 1):
         print(f"{index}: {filename}")
@@ -52,12 +49,19 @@ def list_images():
             if 1 <= img_choice <= len(all_images):
                 selected_image = all_images[img_choice - 1]
                 print(f"Selected Image: {selected_image}")
-                LSBExtract.extract_message(os.path.join('resources/out/', selected_image))
+                print(LSBExtract.extract_message(os.path.join('resources/out/', selected_image)))
+                break
             else:
                 print("Invalid Selection. Please try again.")
         except:
-            print("Please enter a numerical option as listed.")
+            print("\nPlease enter a numerical option as listed.")
+
+    menu()
 
 
 if __name__ == "__main__":
-    menu()
+    try:
+        print("Welcome to the Data Smuggling application!")
+        menu()
+    except KeyboardInterrupt:
+        print("\n\nGoodbye!")
